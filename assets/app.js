b503f2fdd864d98bfbf393cd04d1114d06f5b90b
maia rojas <maia.rt.46@gmail.com>
2017-05-02 00:48:45 -0500

    Realizando la funcionalidad

--- assets/app.js
+++ assets/app.js
@@ -1,11 +1,40 @@
-var cont1= document.getElementById("Enviar");
-var pasw1= document.getElementById("pasw1");
-var pasw2= document.getElementById("psw2");
+var captcha= document.getElementById("captcha");
+var verificar= document.getElementById("verificar");
+var nuevo = document.getElementById("nuevo");
+var check = document.getElementById("check");
 
-cont1.addEventListener("click",function(){
-	if (pasw1.value==pasw2.value){
-		alert("Correcto");
-	}else{
-		alert("Las contraseñas deben de ser iguales");
-	}
+  captcha.value = nuevoCapt();
+  captcha.style.color= color();
+
+check.addEventListener("click", function(){
+
+ if (captcha.value == verificar.value) {
+ 	alert("Correcto...¡¡¡¡")
+ }else{
+ 	alert("El codigo ingresado es incorrecto")
+ }
+
+});
+
+nuevo.addEventListener("click", function(e){
+  e.preventDefault();
+  captcha.value = nuevoCapt();
+  captcha.style.color= color();
 })
+
+function nuevoCapt(){
+	var num="0123456789";
+    var texto="abcdefghijklmnñoqrstuvwxyzABCDEFGHIJKLMNÑOPQRSTUVWXYZ";
+	var vacio="";
+
+	for(var i=0;i<4;i++){
+		var uno= Math.floor(Math.random() * num.length);
+		var dos= Math.floor(Math.random() * texto.length);
+    	vacio += num.substr(uno,1)+texto.substr(dos,1);
+	}
+	return vacio;
+}
+
+function color(){
+	return 'rgb'+ '('+Math.floor(Math.random() * 255)+ ','+Math.floor(Math.random() * 255)+','+Math.floor(Math.random() * 255)+')';
+}
